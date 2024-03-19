fc5fcb638676fb11a26cad82d2924ae8d3bd1e09
Felipe <felipe.daniel.henriquez@gmail.ocm>
2024-03-19 19:09:40 -0300

    feat: muestra lista en formato de array

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -7,13 +7,13 @@ const preguntas = [
         name: 'opcion',
         message: '¿Que desea hacer?',
         choices: [
-            {value: '1', name: "Crear tarea"},  
-            {value: '2', name: "Listar tarea"},
-            {value: '3', name: "Listar tareas completadas"},
-            {value: '4', name: "Listar tareas pendientes"},
-            {value: '5', name: "Completar tarea(s)"},
-            {value: '6', name: "Borrar tarea"},
-            {value: '7', name: "Salir\n"}
+            {value: '1', name: `${"1.".green} Crear tarea`},  
+            {value: '2', name: `${"2.".green} Listar tarea`},
+            {value: '3', name: `${"3.".green} Listar tareas completadas`},
+            {value: '4', name: `${"4.".green} Listar tareas pendientes`},
+            {value: '5', name: `${"5.".green} Completar tarea(s)`},
+            {value: '6', name: `${"6.".green} Borrar tarea`},
+            {value: '0', name: `${"0.".green} Salir\n`}
         ],
         loop: false
     }
